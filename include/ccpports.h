df1758af2d2cd5df110148e1bf66279e9c24e265
Arik Mitschang <arik@arikm.com>
2016-05-10 21:37:14 +0900

    Adding more chips/ports for PWM

--- include/ccpports.h
+++ include/ccpports.h
@@ -1,5 +1,9 @@
 /*
- ports for CCP modules in select microchip chips
+ ports for CCP modules in select microchip chips.
+
+ TMR2ONLY applies if the standard PWM functionality is fixed to
+ timer2. Some chips have extended modules that allow more timers as
+ well as standard, but these macros apply to standard.
 */
 
 #if defined(_16F1936)
@@ -9,4 +13,14 @@
 #if defined(_16F1826)
 #define TRISCCP1 TRISB3
 #define TMR2ONLY
+#endif
+#if defined(_16F1709)
+#define TMR2ONLY
+#define TRISCCP1 TRISC5
+#define TRISCCP2 TRISC3
+#endif
+#if defined(_16F1705)
+#define TMR2ONLY
+#define TRISCCP1 TRISC5
+#define TRISCCP2 TRISC3
 #endif
